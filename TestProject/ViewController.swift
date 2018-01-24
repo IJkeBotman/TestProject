5bdb60a83610fe4582cce1d3e181b539a3f635fe
IJke <ijke@overscope.net>
2018-01-24 14:34:54 +0100

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by IJke Botman on 24/01/2018.
+//  Copyright © 2018 IJke Botman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
